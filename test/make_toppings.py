d76c53a2708783575515e6b98872f474c6c636a6
jackymelonhe <chen193713@163.com>
2018-10-13 14:24:08 +0800

    修改了读取数据的部分

--- test/make_toppings.py
+++ test/make_toppings.py
@@ -1,9 +1,9 @@
 import requests
 def getHTMLTest(url):
     r = requests.get(url,timeout=20)
-    
+
     print(r.status_code)
     print(r.encoding)
     print(r.text)
-
-getHTMLTest('http://baidu.com')
+    print(r.raw.read())
+getHTMLTest('https://api.github.com/events')
